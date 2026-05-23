b5d06bd3a0f1361385eac9f50a8edb8e9c5a2207
MUHAMMAD BILAL MALIK <168167358+Renjioda@users.noreply.github.com>
2026-05-23 17:17:43 +0500

    Create DiscountCalculator.java

---
+++ DiscountCalculator.java
@@ -0,0 +1,7 @@
+public class DiscountCalculator {
+    public static final double REGULAR_DISCOUNT = 10.0;
+    public static final double PREMIUM_DISCOUNT = 2.0;
+    public static final double GUEST_DISCOUNT = 1.0;
+    
+    public double calculateDiscount(UserType type, double bill);
+}
